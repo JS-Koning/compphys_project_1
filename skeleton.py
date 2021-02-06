dd963ea5c570ba4356d276629f79d200536ad6c5
Andre Melo <am@andremelo.org>
2021-02-06 11:13:36 +0100

    add project skeleton

---
+++ skeleton.py
@@ -0,0 +1,152 @@
+"""
+This is a suggestion for structuring your simulation code properly.
+However, it is not set in stone. You may modify it if you feel like
+you have a good reason to do so.
+"""
+
+import numpy as np
+
+
+def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
+    """
+    Molecular dynamics simulation using the Euler or Verlet's algorithms
+    to integrate the equations of motion. Calculates energies and other
+    observables at each timestep.
+
+    Parameters
+    ----------
+    init_pos : np.ndarray
+        The initial positions of the atoms in Cartesian space
+    init_vel : np.ndarray
+        The initial velocities of the atoms in Cartesian space
+    num_tsteps : int
+        The total number of simulation steps
+    timestep : float
+        Duration of a single simulation step
+    box_dim : float
+        Dimensions of the simulation box
+
+    Returns
+    -------
+    Any quantities or observables that you wish to study.
+    """
+
+    return
+
+
+def atomic_distances(pos, box_dim):
+    """
+    Calculates relative positions and distances between particles.
+
+    parameters
+    ----------
+    pos : np.ndarray
+        The positions of the particles in cartesian space
+    box_dim : float
+        The dimension of the simulation box
+
+    returns
+    -------
+    rel_pos : np.ndarray
+        Relative positions of particles
+    rel_dist : np.ndarray
+        The distance between particles
+    """
+
+    return
+
+
+def lj_force(rel_pos, rel_dist):
+    """
+    Calculates the net forces on each atom.
+
+    Parameters
+    ----------
+    rel_pos : np.ndarray
+        Relative particle positions as obtained from atomic_distances
+    rel_dist : np.ndarray
+        Relative particle distances as obtained from atomic_distances
+
+    Returns
+    -------
+    np.ndarray
+        The net force acting on particle i due to all other particles
+    """
+
+    return
+
+
+def fcc_lattice(num_atoms, lat_const):
+    """
+    Initializes a system of atoms on an fcc lattice.
+
+    Parameters
+    ----------
+    num_atoms : int
+        The number of particles in the system
+    lattice_const : float
+        The lattice constant for an fcc lattice
+
+    Returns
+    -------
+    pos_vec : np.ndarray
+        Array of particle coordinates
+    """
+
+    return
+
+
+def kinetic_energy(vel):
+    """
+    Computes the kinetic energy of an atomic system.
+
+    Parameters
+    ----------
+    vel: np.ndarray
+        Velocity of particle
+
+    Returns
+    -------
+    float
+        The total kinetic energy of the system.
+    """
+
+    return
+
+
+def potential_energy(rel_dist):
+    """
+    Computes the potential energy of an atomic system.
+
+    Parameters
+    ----------
+    rel_dist : np.ndarray
+        Relative particle distances as obtained from atomic_distances
+
+    Returns
+    -------
+    float
+        The total potential energy of the system.
+    """
+
+    return
+
+
+def init_velocity(num_atoms, temp):
+    """
+    Initializes the system with Gaussian distributed velocities.
+
+    Parameters
+    ----------
+    num_atoms : int
+        The number of particles in the system.
+    temp : float
+        The (unitless) temperature of the system.
+
+    Returns
+    -------
+    vel_vec : np.ndarray
+        Array of particle velocities
+    """
+
+    return
