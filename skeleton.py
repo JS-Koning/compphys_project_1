1bef426614f746483918a910031da9da07359ad4
Abiram <akanagaratnam@vvtp.tudelft.nl>
2021-02-13 18:46:38 +0100

    started lj_force and made code more pythonic

--- skeleton.py
+++ skeleton.py
@@ -6,7 +6,7 @@ you have a good reason to do so.
 
 import numpy as np
 
-global positions_store, velocities_store;
+global positions_store, velocities_store
 
 # amount of particles
 N = 3
@@ -120,20 +120,21 @@ def atomic_distances(pos, box_dim):
         The distance between particles
     """
 
-    dims = len(pos[0]);
-    rel_pos = np.zeros([len(pos),len(pos),dims])
+    dim = len(pos[0])
+    # NOTE: includes rel_dist/rel_pos to itself (= 0 / [0.0, 0.0])
+    rel_pos = np.zeros([len(pos), len(pos), dim])
 
-    for i in range(0,len(pos)):
-        for j in range(0,len(pos)):
-            for k in range(0,dims):
+    for i in range(0, len(pos)):
+        for j in range(0, len(pos)):
+            for k in range(0, dim):
                 rel_pos[i][j][k] = pos[j][k] - pos[i][k]
 
     rel_dist = np.zeros([len(pos), len(pos)])
-    for i in range(0,len(rel_pos)):
+    for i in range(0, len(rel_pos)):
         for j in range(0, len(rel_pos)):
-             rel_dist[i][j] = np.math.sqrt(sum(i**2 for i in rel_pos[i][j]))
+            rel_dist[i][j] = np.math.sqrt(sum(i**2 for i in rel_pos[i][j]))
 
-    return rel_pos,rel_dist
+    return rel_pos, rel_dist
 
 
 def lj_force(rel_pos, rel_dist):
@@ -152,14 +153,23 @@ def lj_force(rel_pos, rel_dist):
     np.ndarray
         The net force acting on particle i due to all other particles
     """
-    F = 0
+    force = np.zeros(len(rel_dist))
 
-    return
-    for i in len(rel_dist):
-        dUdr = 0
-        F -= dUdr
+    for i in range(0, len(rel_dist)):
+        for j in range(0, len(rel_dist[i])):
+            # does something with rel_dist
+            if rel_dist[i][j] == 0.0:
+                # do not include contributions to self
+                continue
 
-    return F
+            du_dr = 1/rel_dist[i][j]
+            force[i] -= du_dr
+
+    return force
+
+
+# x,y = atomic_distances(positions, M)
+# print(lj_force(x,y))
 
 
 def fcc_lattice(num_atoms, lat_const):
