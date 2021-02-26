da1550a3798e1fbc857ce326e30d5dbacc879d36
Abiram <akanagaratnam@vvtp.tudelft.nl>
2021-02-26 13:21:05 +0100

    implemented verlet, updated custom init pos/vel

--- skeleton.py
+++ skeleton.py
@@ -10,35 +10,34 @@ import matplotlib.pyplot as plt
 global positions_store, velocities_store
 
 # initalizing self defined system parameters
-num_atoms = 2               # amount of particles
-dim = 3                     # dimensions
-box_dim = 10                # meters; bounding box dimension
-dt = 1e-4                   # s; stepsize
-steps = 10000                # amount of steps
-dimless = True              # use dimensionless units
-periodic = True             # use periodicity
-verlet = True               # use Verlet's algorithm
+num_atoms = 2  # amount of particles
+dim = 3  # dimensions
+box_dim = 10  # meters; bounding box dimension
+dt = 1e-4  # s; stepsize
+steps = 10000  # amount of steps
+dimless = True  # use dimensionless units
+periodic = True  # use periodicity
+verlet = False  # use Verlet's algorithm
 
 # Parameters physical, supplied by course, or related to Argon
-temp = 119.8                # Kelvin
-KB = 1.38064852e-23         # m^2*kg/s^2/K
-SIGMA = 3.405e-10           # meter
-EPSILON = temp * KB         # depth of potential well/dispersion energy
-N_b = 6.02214076e23         # Avagadros number; 1/mol
-R = 8.31446261815324        # J/K/mole; universal gas constant
-ARG_UMASS = 39.95           # u; atomic mass of argon
-ARG_MMASS = ARG_UMASS/1000  # kg/mol; mole mass of argon
-ARG_MASS = ARG_UMASS*1.6605e-27 #Kg mass of a single atom in Kg
+temp = 119.8  # Kelvin
+KB = 1.38064852e-23  # m^2*kg/s^2/K
+SIGMA = 3.405e-10  # meter
+EPSILON = temp * KB  # depth of potential well/dispersion energy
+N_b = 6.02214076e23  # Avagadros number; 1/mol
+R = 8.31446261815324  # J/K/mole; universal gas constant
+ARG_UMASS = 39.95  # u; atomic mass of argon
+ARG_MMASS = ARG_UMASS / 1000  # kg/mol; mole mass of argon
+ARG_MASS = ARG_UMASS * 1.6605e-27  # Kg mass of a single atom in Kg
 
 # conversion values for dimensionless units
-dimless_time = 1.0 / np.math.sqrt((ARG_MASS*SIGMA**2/EPSILON)) # s; dimensionless time
-dimless_energy = 1.0 / EPSILON                                      # J; dimensionless energy
-dimless_distance = 1.0 / SIGMA                                      # m; dimensionless distance
-dimless_velocity = 1.0 / np.math.sqrt(EPSILON/(ARG_MASS))      # m/s; dimensionless velocity
+dimless_time = 1.0 / np.math.sqrt((ARG_MASS * SIGMA ** 2 / EPSILON))  # s; dimensionless time
+dimless_energy = 1.0 / EPSILON  # J; dimensionless energy
+dimless_distance = 1.0 / SIGMA  # m; dimensionless distance
+dimless_velocity = 1.0 / np.math.sqrt(EPSILON / (ARG_MASS))  # m/s; dimensionless velocity
+
 
 def init_velocity(num_atoms, temp, dim):
-    
-    
     """
     Initializes the system with Gaussian distributed velocities. This
     init_velocity is loosely based on 3D system, however it will output 2D just
@@ -62,16 +61,16 @@ def init_velocity(num_atoms, temp, dim):
         Array of particle velocities
     """
     # define most probable speed (vel_p), then use this to find mean speed, using Maxwell-Boltzmann distribution
-    vel_p = np.sqrt(2*KB*temp/ARG_MASS)
+    vel_p = np.sqrt(2 * KB * temp / ARG_MASS)
 
     if dimless:
         vel_p *= dimless_velocity
 
-    vel_mean = 2*vel_p/np.sqrt(np.pi)
+    vel_mean = 2 * vel_p / np.sqrt(np.pi)
     # define the standard deviation, assuming the standard deviation: std = sqrt(meansquarevelocity^2 - mean velocity^2) 
     # again, using Maxwell-Boltzmann distributions
-    vel_msq = (3*vel_p**2)/2
-    vel_std = vel_msq-(vel_mean**2)
+    vel_msq = (3 * vel_p ** 2) / 2
+    vel_std = vel_msq - (vel_mean ** 2)
     # find the final distribution
     vel_vec = np.random.normal(vel_mean, vel_std, (num_atoms, dim))
     return vel_vec
@@ -101,10 +100,10 @@ def init_position(num_atoms, box_dim, dim):
     random = np.random.random((num_atoms, dim))
 
     # to test close to boundary
-    #random *= 0.001
+    # random *= 0.001
 
     pos_vec = random * box_dim
-    
+
     return pos_vec
 
 
@@ -131,35 +130,36 @@ def simulate_old(init_pos, init_vel, num_tsteps, timestep, box_dim):
     -------
     Any quantities or observables that you wish to study.
     """
-     # first initialize matrix starting with initial velocity and position
+    # first initialize matrix starting with initial velocity and position
     pos_steps = np.zeros((num_tsteps, num_atoms, dim))
     vel_steps = np.zeros((num_tsteps, num_atoms, dim))
 
     pos_steps[0, :, :] = init_pos
     vel_steps[0, :, :] = init_vel
-    for i in range(num_tsteps-1):
+    for i in range(num_tsteps - 1):
         pos = pos_steps[i, :, :]
 
         # make sure it's inside box dimension -> modulus gives periodicity
         if periodic:
             if dimless:
-                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep*dimless_time) % box_dim*dimless_distance
+                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :,
+                                                :] * timestep * dimless_time) % box_dim * dimless_distance
             else:
-                pos_steps[i+1, :, :] = (pos + vel_steps[i, :, :] * timestep ) % box_dim
+                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep) % box_dim
         else:
             if dimless:
-                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep*dimless_time)
+                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep * dimless_time)
             else:
-                pos_steps[i+1, :, :] = (pos + vel_steps[i, :, :] * timestep )
+                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep)
 
         rel_pos = atomic_distances(pos, box_dim)[0]
         rel_dis = atomic_distances(pos, box_dim)[1]
         force = lj_force(rel_pos, rel_dis)[1]
 
         if dimless:
-            vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep*dimless_time / ARG_MASS
+            vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep * dimless_time / ARG_MASS
         else:
-            vel_steps[i+1, :, :] = vel_steps[i, :, :] + force * timestep / ARG_MASS
+            vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep / ARG_MASS
 
     global positions_store
     positions_store = pos_steps
@@ -167,6 +167,7 @@ def simulate_old(init_pos, init_vel, num_tsteps, timestep, box_dim):
     velocities_store = vel_steps
 
     return pos_steps, vel_steps
+
 
 def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
     """
@@ -191,35 +192,64 @@ def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
     -------
     Any quantities or observables that you wish to study.
     """
-     # first initialize matrix starting with initial velocity and position
+    # first initialize matrix starting with initial velocity and position
     pos_steps = np.zeros((num_tsteps, num_atoms, dim))
     vel_steps = np.zeros((num_tsteps, num_atoms, dim))
 
     pos_steps[0, :, :] = init_pos
     vel_steps[0, :, :] = init_vel
-    for i in range(num_tsteps-1):
+    for i in range(num_tsteps - 1):
         pos = pos_steps[i, :, :]
 
-        # make sure it's inside box dimension -> modulus gives periodicity
-        if periodic:
-            if dimless:
-                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep) % box_dim
-            else:
-                pos_steps[i+1, :, :] = (pos + vel_steps[i, :, :] * timestep ) % box_dim
-        else:
-            if dimless:
-                pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep )
-            else:
-                pos_steps[i+1, :, :] = (pos + vel_steps[i, :, :] * timestep )
+        if verlet:
+            # Verlet velocity algorithm
+            rel_pos, rel_dis = atomic_distances(pos, box_dim)
+            force = lj_force(rel_pos, rel_dis)[1]
 
-        rel_pos = atomic_distances(pos, box_dim)[0]
-        rel_dis = atomic_distances(pos, box_dim)[1]
-        force = lj_force(rel_pos, rel_dis)[1]
+            if periodic:
+                # make sure it's inside box dimension -> modulus gives periodicity
+                if dimless:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep + (
+                                timestep ** 2) * force / 2) % box_dim
+                else:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep + (
+                                timestep ** 2) * force / 2) % box_dim
+            else:
+                if dimless:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep + (timestep ** 2) * force / 2)
+                else:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep + (timestep ** 2) * force / 2)
 
-        if dimless:
-            vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep
+            # force after position update (needed for verlet velocity)
+            new_rel_pos, new_rel_dis = atomic_distances(pos_steps[i + 1, :, :], box_dim)
+            new_force = lj_force(new_rel_pos, new_rel_dis)[1]
+
+            if dimless:
+                vel_steps[i + 1, :, :] = vel_steps[i, :, :] + timestep * (new_force + force) / 2
+            else:
+                vel_steps[i + 1, :, :] = vel_steps[i, :, :] + timestep * (new_force + force) / 2 / ARG_MASS
         else:
-            vel_steps[i+1, :, :] = vel_steps[i, :, :] + force * timestep / ARG_MASS
+            # Euler
+            if periodic:
+                # make sure it's inside box dimension -> modulus gives periodicity
+                if dimless:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep) % box_dim
+                else:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep) % box_dim
+            else:
+                if dimless:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep)
+                else:
+                    pos_steps[i + 1, :, :] = (pos + vel_steps[i, :, :] * timestep)
+
+            rel_pos = atomic_distances(pos, box_dim)[0]
+            rel_dis = atomic_distances(pos, box_dim)[1]
+            force = lj_force(rel_pos, rel_dis)[1]
+
+            if dimless:
+                vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep
+            else:
+                vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep / ARG_MASS
 
     global positions_store
     positions_store = pos_steps
@@ -227,7 +257,6 @@ def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
     velocities_store = vel_steps
 
     return pos_steps, vel_steps
-
 
 
 def atomic_distances(pos, box_dim):
@@ -273,7 +302,7 @@ def atomic_distances(pos, box_dim):
     rel_dist = np.zeros([len(pos), len(pos)])
     for i in range(0, len(rel_pos)):
         for j in range(0, len(rel_pos)):
-            rel_dist[i][j] = np.math.sqrt(sum(i**2 for i in rel_pos[i][j]))
+            rel_dist[i][j] = np.math.sqrt(sum(i ** 2 for i in rel_pos[i][j]))
 
     return rel_pos, rel_dist
 
@@ -305,15 +334,17 @@ def lj_force(rel_pos, rel_dist):
     dUdt = np.zeros([len(rel_dist), len(rel_dist)])
     force = np.zeros([len(rel_pos[1]), len(rel_pos[1]), len(rel_pos[0][0])])
 
-    for i in range (0,len(rel_pos[1])): #particle i
-        for j in range (0, len(rel_pos[1])): #particle i rel to j (!=i)
+    for i in range(0, len(rel_pos[1])):  # particle i
+        for j in range(0, len(rel_pos[1])):  # particle i rel to j (!=i)
             if i != j:
-                dUdt[i, j] = -24*EPSILON*((2*SIGMA**12/(rel_dist[i, j]**13)) - (SIGMA**6/rel_dist[i, j]**13))/(rel_dist[i, j]) 
+                dUdt[i, j] = -24 * EPSILON * (
+                        (2 * SIGMA ** 12 / (rel_dist[i, j] ** 13)) - (SIGMA ** 6 / rel_dist[i, j] ** 13)) / (
+                                 rel_dist[i, j])
             else:
                 dUdt[i, j] = 0
-    for i in range (0,len(rel_pos[1])): #particle i
-        for j in range (0, len(rel_pos[1])): #particle i rel to j (!=i)
-            force[i, j, :] = dUdt[i, j]*rel_pos[i, j, :]
+    for i in range(0, len(rel_pos[1])):  # particle i
+        for j in range(0, len(rel_pos[1])):  # particle i rel to j (!=i)
+            force[i, j, :] = dUdt[i, j] * rel_pos[i, j, :]
     # while this looks horrible, and is horrible, it works. However, needs significant optimazation
 
     force_atom = np.sum(force, axis=1)
@@ -394,21 +425,21 @@ def potential_energy(rel_dist):
     for j in range(0, num_atoms1):
         for i in range(0, num_atoms1):
             if i != j:
-                pot_e[i][j] = 4*EPSILON*((SIGMA/rel_dist[i][j])**12-(SIGMA/rel_dist[i][j])**6)
+                pot_e[i][j] = 4 * EPSILON * ((SIGMA / rel_dist[i][j]) ** 12 - (SIGMA / rel_dist[i][j]) ** 6)
             else:
                 pot_e[i][j] = 0
     pot_e_particle = np.sum(pot_e, axis=1)
-    pot_total = np.sum(pot_e_particle)/2
+    pot_total = np.sum(pot_e_particle) / 2
 
     if dimless:
         for j in range(0, num_atoms1):
             for i in range(0, num_atoms1):
                 if i != j:
-                    pot_e[i][j] = 4*((1/rel_dist[i][j])**12-(1/rel_dist[i][j])**6)
+                    pot_e[i][j] = 4 * ((1 / rel_dist[i][j]) ** 12 - (1 / rel_dist[i][j]) ** 6)
                 else:
                     pot_e[i][j] = 0
         pot_e_particle = np.sum(pot_e, axis=1)
-        pot_total = np.sum(pot_e_particle)/2
+        pot_total = np.sum(pot_e_particle) / 2
 
     return pot_e, pot_e_particle, pot_total
 
@@ -449,25 +480,24 @@ def main():
     init_vel = init_velocity(num_atoms, box_dim, dim)
 
     #    easy, handpicked initial positions and velocities.
-    init_pos = [[9.9, 9.8, 8.7], [0.3, 0.6, 0.3]]
-    init_vel = [[1.0, 1.0, 1.2], [-1.0, -1.2, -0.9]]
+    init_pos = [[9.9, 9.6, 8.7], [0.3, 0.6, 0.3]]
+    init_vel = [[1.2, 0.9, 1.2], [-0.9, -0.9, -0.6]]
 
-    
     simulate(init_pos, init_vel, steps, dt, box_dim)
 
     print("Test if the total energy is conserved")
     pos1 = positions_store[0, :, :]
-    pos2 = positions_store[steps-1, :, :]
+    pos2 = positions_store[steps - 1, :, :]
 
     vel1 = velocities_store[0, :, :]
-    vel2 = velocities_store[steps-1, :, :]
+    vel2 = velocities_store[steps - 1, :, :]
 
     r_pos1 = atomic_distances(pos1, box_dim)
     r_pos2 = atomic_distances(pos2, box_dim)
 
     print("Initial total energy: " + str(total_energy(vel1, r_pos1[1])))
     print("Final total energy:   " + str(total_energy(vel2, r_pos2[1])))
-    print("Delta total energy:   " + str(total_energy(vel2, r_pos2[1])-total_energy(vel1, r_pos1[1])))
+    print("Delta total energy:   " + str(total_energy(vel2, r_pos2[1]) - total_energy(vel1, r_pos1[1])))
 
     if num_atoms == 2:
         print("Plot inter-atom distance over time")
@@ -475,7 +505,7 @@ def main():
             distances = [np.max(atomic_distances(positions_store[x, :, :], box_dim)[1]) for x in range(steps)]
         else:
             distances = [np.max(atomic_distances(positions_store[x, :, :], box_dim)[1]) for x in range(steps)]
-        times = np.linspace(0, dt*steps, steps)
+        times = np.linspace(0, dt * steps, steps)
         plt.plot(times, distances)
         if dimless:
             plt.ylabel('Distance (nondim)')
@@ -489,8 +519,10 @@ def main():
 
         print("Print energy levels over time")
         if dimless:
-            energies = [(kinetic_energy(velocities_store[x, :, :]), potential_energy(atomic_distances(positions_store[x, :, :],box_dim)[1])[2],
-                        total_energy(velocities_store[x, :, :], atomic_distances(positions_store[x, :, :],box_dim)[1]))
+            energies = [(kinetic_energy(velocities_store[x, :, :]),
+                         potential_energy(atomic_distances(positions_store[x, :, :], box_dim)[1])[2],
+                         total_energy(velocities_store[x, :, :],
+                                      atomic_distances(positions_store[x, :, :], box_dim)[1]))
                         for x in range(steps)]
         else:
             energies = [kinetic_energy(velocities_store[x, :, :]) for x in range(steps)]
@@ -500,5 +532,5 @@ def main():
         plt.ylabel('Energy (dimless)')
         plt.show()
 
-main()
 
+main()
