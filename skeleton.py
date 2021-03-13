9ff1e34cf88421eba0c5f9fb0400c4fc4efd7033
Abiram <akanagaratnam@vvtp.tudelft.nl>
2021-03-13 15:48:33 +0100

    rescaling fixes

--- skeleton.py
+++ skeleton.py
@@ -11,14 +11,16 @@ import matplotlib.pyplot as plt
 global positions_store, velocities_store
 
 # initalizing self defined system parameters
-num_atoms = 2  # amount of particles
+num_atoms = 4  # amount of particles
 dim = 3  # dimensions
-box_dim = 10  # meters; bounding box dimension
+box_dim = 1.547#10  # meters; bounding box dimension
 dt = 1e-4  # s; stepsize
-steps = 60000  # amount of steps
+steps = 90000  # amount of steps
 dimless = True  # use dimensionless units
 periodic = True  # use periodicity
 verlet = True  # use Verlet's algorithm
+rescaling = False # use Temperature rescaling
+rescaling_mode = 0 # 0 = kin-NRG-based | 1 = tot-NRG-based
 
 # Parameters physical, supplied by course, or related to Argon
 temp = 30  # Kelvin
@@ -321,23 +323,30 @@ def simulate(init_pos, init_vel, num_tsteps, timestep, box_dim):
             else:
                 vel_steps[i + 1, :, :] = vel_steps[i, :, :] + force * timestep / ARG_MASS
 
-        # Rescale velocity
-        average_kin = np.sum([kinetic_energy(vel_steps[i-x, :, :])[1] for x in range(min(i+1,10))])/min(i+1,10)
-        average_kin_new = np.sum([kinetic_energy(vel_steps[i+1-x, :, :])[1] for x in range(min(i + 1, 10))]) / min(i + 1, 10)
+        if rescaling:
+            # Rescale velocity
+            if rescaling_mode == 0:
+                average_nrg = np.sum([kinetic_energy(vel_steps[i-x, :, :])[1] for x in range(min(i+1,10))])/min(i+1,10)
+                average_nrg_new = np.sum([kinetic_energy(vel_steps[i+1-x, :, :])[1] for x in range(min(i + 1, 10))]) / min(i + 1, 10)
+            else:
+                average_nrg = np.sum([total_energy(vel_steps[i - x, :, :],atomic_distances(pos_steps[i - x, :, :], box_dim)[1]) for x in range(min(i + 1, 10))]) / min(i + 1, 10)
+                average_nrg_new = np.sum([total_energy(vel_steps[i+1-x, :, :],atomic_distances(pos_steps[i+1-x, :, :], box_dim)[1]) for x in range(min(i + 1, 10))]) / min(i + 1, 10)
 
-        if np.abs(average_kin_new - average_kin) < 0.0003:
-            #v_lambda = np.sqrt((num_atoms - 1) * 3 * KB * temp / (ARG_MASS * np.sum([np.sqrt(np.sum([v[i] ** 2 for i in range(dim)])) for v in vel_steps[i + 1, :, :]]) * dimless_velocity))/1500
-            #v_lambda = np.sqrt((num_atoms - 1) * 3 * KB * temp / (ARG_MASS * np.sum([np.sqrt(np.sum([v[i] ** 2 for i in range(dim)])) for v in vel_steps[i + 1, :, :]]))) / TEMP
-            v_lambda = np.sqrt((num_atoms - 1) * 3 * KB * temp / (EPSILON * np.sum([np.sqrt(np.sum([v[i] ** 2 for i in range(dim)])) for v in vel_steps[i + 1, :, :]]))) # / TEMP
-            v_lambda = max(0.5,v_lambda)
-            v_lambda = min(2.0,v_lambda)
-            #vel_steps[i + 1, :, :] *= v_lambda
-            rescale_counter+=1
-            rescale_max = max(rescale_max,v_lambda)
-            rescale_min = min(rescale_min,v_lambda)
-            #print("Rescale:",v_lambda)
+            if np.abs(average_nrg_new - average_nrg) < 0.00000001 * temp:
+                #v_lambda = np.sqrt((num_atoms - 1) * 3 * KB * temp / (ARG_MASS * np.sum([np.sqrt(np.sum([v[i] ** 2 for i in range(dim)])) for v in vel_steps[i + 1, :, :]]) * dimless_velocity))/1500
+                #v_lambda = np.sqrt((num_atoms - 1) * 3 * KB * temp / (ARG_MASS * np.sum([np.sqrt(np.sum([v[i] ** 2 for i in range(dim)])) for v in vel_steps[i + 1, :, :]]))) / TEMP
+                v_lambda = np.sqrt((num_atoms - 1) * 3 * KB * temp / (EPSILON * np.sum([np.sqrt(np.sum([v[i] ** 2 for i in range(dim)])) for v in vel_steps[i + 1, :, :]]))) # / TEMP
+                v_lambda = max(0.5,v_lambda)
+                v_lambda = min(2.0,v_lambda)
+                vel_steps[i + 1, :, :] *= v_lambda
+                rescale_counter+=1
+                rescale_max = max(rescale_max,v_lambda)
+                rescale_min = min(rescale_min,v_lambda)
+                #print("Rescale:",v_lambda)
 
-    print("Rescaled",rescale_counter,"times [",rescale_min,"~",rescale_max,"]")
+    if rescaling:
+        print("Rescaled",rescale_counter,"times [",rescale_min,"~",rescale_max,"]")
+
     global positions_store
     positions_store = pos_steps
     global velocities_store
@@ -800,11 +809,11 @@ def main():
     #    , [-0.22, 1.53, -0.34], [1.25, 0.66, -0.97], [-0.36, -1.29, 0.09], [1.22, 0.01, -0.61]]
 
     # Below is the must be uncommented for the delivarble.
-    #init_pos = fcc_lattice(num_atoms, 1.547)
-    #init_vel = init_velocity(num_atoms,temp,dim)
+    init_pos = fcc_lattice(num_atoms, 1.547)
+    init_vel = init_velocity(num_atoms,temp,dim)
 
-    init_pos = [[5,5,5], [6,5,5]]
-    init_vel = [[0,0,0], [0,0,0]]
+    #init_pos = [[5,5,5], [6,5,5]]
+    #init_vel = [[0,0,0], [0,0,0]]
 
     test_initial_velocities(init_vel)
 
